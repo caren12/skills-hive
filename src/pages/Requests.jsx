8eee2aa4792544b68abc5f2c22a4479f76fb8803
Brian mutai <bkmutai68w@gmail.com>
2026-05-14 12:04:30 +0300

    Created request.jsx

---
+++ src/pages/Requests.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+function Requests() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container">
+        <h2>Skill Swap Requests</h2>
+        <p>No requests yet</p>
+      </div>
+    </>
+  );
+}
+
+export default Requests;
